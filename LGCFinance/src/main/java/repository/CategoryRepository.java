38bdd74d8fca5dda6f04db0b331a659b5f8d0d37
gusdev0258 <16326176760@edu.udesc.br>
2024-01-12 16:41:41 -0300

    create category and payment repository

--- LGCFinance/src/main/java/repository/CategoryRepository.java
+++ LGCFinance/src/main/java/repository/CategoryRepository.java
@@ -4,10 +4,36 @@
  */
 package repository;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
+import model.Category;
+
 /**
  *
  * @author sonho
  */
 public class CategoryRepository {
     
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public Category saveAuthor(Category category) {
+        if(category.getId() == null) {
+            entityManager.persist(category);
+        } else {
+            category = entityManager.merge(category);
+        }
+        return category;
+    }
+
+    public List<Category> getAllCategories() {
+       return entityManager.createQuery("SELECT * FROM category", Category.class).getResultList();
+    }
+
+    public Optional<Category> getById(Long id) {
+        Category category = entityManager.find(Category.class, id);
+        return Optional.ofNullable(category);
+    }
 }
--- LGCFinance/src/main/java/repository/PaymentRepository.java
+++ LGCFinance/src/main/java/repository/PaymentRepository.java
@@ -4,10 +4,36 @@
  */
 package repository;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
+import model.Payment;
+
 /**
  *
  * @author sonho
  */
 public class PaymentRepository {
     
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public Payment saveAuthor(Payment payment) {
+        if(payment.getId() == null) {
+            entityManager.persist(payment);
+        } else {
+            payment = entityManager.merge(payment);
+        }
+        return payment;
+    }
+
+    public List<Payment> getAllPayments() {
+       return entityManager.createQuery("SELECT * FROM payment", Payment.class).getResultList();
+    }
+
+    public Optional<Payment> getById(Long id) {
+        Payment payment = entityManager.find(Payment.class, id);
+        return Optional.ofNullable(payment);
+    }
 }
